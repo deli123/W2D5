b62fdcd9a0048beff58109ce3dc57fa7a007da53
Derek <liderek63@gmail.com>
2022-06-24 14:35:45 -0700

    completed item.rb

---
+++ todoboard/item.rb
@@ -0,0 +1,34 @@
+class Item
+    attr_accessor :title, :deadline, :description
+
+    def self.valid_date?(date_string)
+        year = date_string[0..3].to_i
+        month = date_string[5..6].to_i
+        day = date_string[8..9].to_i
+        if !(month >= 1 && month <= 12)
+            return false
+        end
+        if !(day >= 1 && day <= 30)
+            return false
+        end
+        return true
+    end 
+
+    def initialize(title, deadline, description)
+        if !Item.valid_date?(deadline)
+            raise "Invalid date."
+        end
+
+        @title = title
+        @deadline = deadline
+        @description = description
+    end
+
+    def deadline=(deadline)
+        if !Item.valid_date?(deadline)
+            raise "Invalid deadline."
+        else
+            @deadline = deadline
+        end
+    end
+end
