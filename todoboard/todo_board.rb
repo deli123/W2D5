792bcd640c4d86aee396bbb3b558d1c6882c73a4
Derek <liderek63@gmail.com>
2022-06-24 15:53:03 -0700

    fixed str to int conversion

--- todoboard/todo_board.rb
+++ todoboard/todo_board.rb
@@ -1,1 +1,44 @@
+require_relative "list"
+
 class TodoBoard
+    def initialize(label)
+        @list = List.new(label)
+    end
+
+    def get_command
+        print "\nEnter a command: "
+        cmd, *args = gets.chomp.split(' ')
+
+        case cmd
+        when 'mktodo'
+            @list.add_item(*args)
+        when 'up'
+            @list.up(*args.map!(&:to_i))
+        when 'down'
+            @list.down(*args.map!(&:to_i))
+        when 'swap'
+            @list.swap(*args.map!(&:to_i))
+        when 'sort'
+            @list.sort_by_date!
+        when 'priority'
+            @list.print_priority
+        when 'print'
+            if args.length == 0
+                @list.print
+            else
+                @list.print_full_item(args[0].to_i)
+            end
+        when 'quit'
+            return false
+        else
+            print "Sorry, that command is not recognized."
+        end
+
+        true
+    end
+
+    def run
+        while self.get_command
+        end
+    end
+end
